29dec41a536c48dcd30fec2d1cfb0523a8108d07
Henrique Reinaldi <carlosofalmeida@gmail.com>
2026-09-13 12:16:58 -0300

    Add files via upload

--- ferramentas/bandit_ast.py
+++ ferramentas/bandit_ast.py
@@ -13,5 +13,5 @@ result = codigo_legal("BANDIT")
 
 """
 
-parsed_ast = ast.parse(python_code)
-print(ast.dump(parsed_ast, indent=2))
+arvore = ast.parse(python_code)
+print(ast.dump(arvore, indent=2))
--- ideia/apresentacaoOAUTH/asa.py
+++ ideia/apresentacaoOAUTH/asa.py
@@ -3,9 +3,9 @@ import ast
 python_code = ""
 with open("github.py", "r") as f:
     python_code = f.read()
-parsed_ast = ast.parse(python_code)
+arvore = ast.parse(python_code)
 
-assignments = [no for no in ast.walk(parsed_ast) if isinstance(no, ast.Assign)]
+assignments = [no for no in ast.walk(arvore) if isinstance(no, ast.Assign)]
 for assignment in assignments:
     target = assignment.targets[0]
     if not isinstance(target, ast.Name):
